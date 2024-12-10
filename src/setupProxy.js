98de812aaff6c1fce67b2e790aec5e40b1b7c5b3
Richardo Hohmann <richardohohmann8@outlook.de>
2024-12-10 15:18:43 +0100

    -

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/api/v1',
     createProxyMiddleware({
       target: 'http://127.0.0.1:51515',
       changeOrigin: true,
